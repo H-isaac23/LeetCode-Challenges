60ef7453cb1344fa1c9a5db0222654a785f23901
John Isaac Delgado <ji23delgado@gmail.com>
2021-04-11 20:06:49 +0800

    Working solution, might be too slow

--- April 11.py
+++ April 11.py
@@ -12,3 +12,37 @@ Constraints:
 
 The number of nodes in the tree is in the range [1, 104].
 1 <= Node.val <= 100"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: TreeNode) -> int:
+        self.vals = []
+        self.helper(root, 1, root)
+        self.vals.pop(0)
+        return sum(self.vals) // 2
+
+    def helper(self, node, d, bn):
+        if node is None:
+            if self.vals == []:
+                self.vals.append(d)
+                self.vals.append(bn.val)
+            else:
+                if d > self.vals[0]:
+                    self.vals.clear()
+                    self.vals.append(d)
+                    self.vals.append(bn.val)
+                elif d == self.vals[0]:
+                    self.vals.append(bn.val)
+            print(self.vals)
+            return
+        print(node.val)
+
+        self.helper(node.right, d + 1, node)
+        self.helper(node.left, d + 1, node)
+
