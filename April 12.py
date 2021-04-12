385438c123d75a19d530dcd27059a82228aa5ad6
John Isaac Delgado <ji23delgado@gmail.com>
2021-04-12 20:43:53 +0800

    GH: Fastest solution

--- April 12.py
+++ April 12.py
@@ -61,3 +61,22 @@ class Solution2:
                 N.append(w)
 
         return N
+
+class Solution3:
+    def constructArray(self, n: int, k: int) -> List[int]:
+        ans = [0] * n
+        a = 1
+        z = k+1
+        for i in range(k+1):
+            if i % 2:
+                ans[i] = z
+                z -= 1
+            else:
+                ans[i] = a
+                a += 1
+        for i in range(k+1,n):
+            ans[i] = i + 1
+        return ans
+
+# Submission Details:
+# >97%/>93%
