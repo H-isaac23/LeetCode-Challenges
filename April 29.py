67ec9f4d78b7c818e367c10a1db7e168cba88608
John Isaac Delgado <ji23delgado@gmail.com>
2021-04-29 18:21:33 +0800

    SM: Accepted but slow solution

--- April 29.py
+++ April 29.py
@@ -22,3 +22,27 @@ Constraints:
 -109 <= nums[i] <= 109
 nums is a non-decreasing array.
 -109 <= target <= 109"""
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if nums == []:
+            return [-1, -1]
+        vals = []
+        for i in range(len(nums)):
+            if nums[i] == target and vals == []:
+                vals.append(i)
+            elif i == len(nums) - 1 and len(vals) == 1 and nums[i] == target:
+                vals.append(i)
+            elif nums[i] == target and nums[i + 1] != target:
+                vals.append(i)
+
+            if i == len(nums) - 1 and vals == []:
+                return [-1, -1]
+            elif i == len(nums) - 1 and len(vals) == 1:
+                vals.append(vals[0])
+
+        return vals
+
+# Submission Details
+# >5.79%
