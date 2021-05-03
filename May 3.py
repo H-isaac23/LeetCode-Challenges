20b9a2c291a49691e7d2fc94e054c5bf88ae3664
John Isaac Delgado <ji23delgado@gmail.com>
2021-05-03 22:56:54 +0800

    GH: Improved solution

--- May 3.py
+++ May 3.py
@@ -28,3 +28,13 @@ class Solution:
         return sums
 
 # >60%/>40%
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        sums = [0]*len(nums)
+        sums[0] = nums[0]
+        for i in range(1, len(nums)):
+            sums[i] = sums[i-1] + nums[i]
+        return sums
+
+# Improved
