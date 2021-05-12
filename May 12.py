1023c4144ba51807384c49de29889fdb3146af5b
John Isaac Delgado <ji23delgado@gmail.com>
2021-05-12 19:50:07 +0800

    SM: Faster solution

--- May 12.py
+++ May 12.py
@@ -51,3 +51,17 @@ class NumMatrix:
 
 # Submission Details
 # >10%/>90%
+
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        self.matrix = matrix
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        ans = 0
+        for i in range(row1, row2+1):
+            ans += sum(self.matrix[i][col1:col2+1])
+        return ans
+
+# Submission Details
+# >16%/90%
