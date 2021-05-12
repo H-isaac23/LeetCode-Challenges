ba4bbfd91a47476fa91763d27238b165afc52453
John Isaac Delgado <ji23delgado@gmail.com>
2021-05-12 19:47:45 +0800

    SM: Accepted solution

--- May 12.py
+++ May 12.py
@@ -33,3 +33,21 @@ n == matrix[i].length
 0 <= col1 <= col2 < n
 At most 104 calls will be made to sumRegion."""
 
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        self.matrix = matrix
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        ans = 0
+        for i in range(row1, row2+1):
+            for j in range(col1, col2+1):
+                ans += self.matrix[i][j]
+        return ans
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
+
+# Submission Details
+# >10%/>90%
