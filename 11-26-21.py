b3bf1531d6f0f31b0c5c3c0e4271989a91f86c96
H-isaac23 <dev.isaac23@gmail.com>
2021-11-26 22:58:02 +0800

    real 11-26-21

--- 11-26-21.py
+++ 11-26-21.py
@@ -12,3 +12,18 @@ class Solution:
             dp[i] = max(nums[i], nums[i] + dp[i - 1])
 
         return max(dp)
+
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+#
+# You must write an algorithm with O(log n) runtime complexity.
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+            if nums[i] > target:
+                return i
+
+        return len(nums)
