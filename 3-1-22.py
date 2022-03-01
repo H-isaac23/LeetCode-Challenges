7cbdd6ce3be056833108dab05d13b5695616022d
H-isaac23 <dev.isaac23@gmail.com>
2022-03-01 21:47:53 +0800

    3-1-22 sol

---
+++ 3-1-22.py
@@ -0,0 +1,44 @@
+# Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of
+# 1's in the binary representation of i.
+#
+# Example 1:
+# Input: n = 2
+# Output: [0,1,1]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+#
+# Example 2:
+# Input: n = 5
+# Output: [0,1,1,2,1,2]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+# 3 --> 11
+# 4 --> 100
+# 5 --> 101
+#
+# Constraints:
+# 0 <= n <= 105
+#
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = []
+
+        for i in range(n + 1):
+            if i == 0:
+                res.append(0)
+            elif i == 1:
+                res.append(1)
+            else:
+                num_ones = 0
+                num = i
+                while num > 0:
+                    num_ones += num % 2
+                    num //= 2
+                res.append(num_ones)
+
+        return res
+
