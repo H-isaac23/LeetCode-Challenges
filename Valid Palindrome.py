dcce6c2c45cccd39a45d4b9874561c12f2990cc6
H-isaac23 <dev.isaac23@gmail.com>
2022-03-20 00:05:18 +0800

    valid pal sol

---
+++ Valid Palindrome.py
@@ -0,0 +1,44 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all
+# non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and
+# numbers.
+# Given a string s, return true if it is a palindrome, or false otherwise.
+#
+# Example 1:
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+#
+# Example 2:
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+#
+# Example 3:
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+#
+# Constraints:
+# 1 <= s.length <= 2 * 105
+# s consists only of printable ASCII characters.
+#
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # ord
+        s = s.lower()
+        l, r = 0, len(s) - 1
+        while l <= r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            elif s[r] != s[l]:
+                print(r, l)
+                return False
+            else:
+                l += 1
+                r -= 1
+        return True
+
